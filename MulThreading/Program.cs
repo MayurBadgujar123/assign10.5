066e7cb0d55987727af7043deec685d10fe7df19
MayurBadgujar123 <132994310+MayurBadgujar123@users.noreply.github.com>
2023-08-31 22:11:07 +0530

    Add files via upload

---
+++ MulThreading/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace MulThreading
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
